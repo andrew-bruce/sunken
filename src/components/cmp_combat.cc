2240b519eff5f679a659c5e58c5cedf5556a4993
WalterPlinge <22519813+WalterPlinge@users.noreply.github.com>
2019-04-18 00:27:29 +0100

    Changed player and torpedo size to be relative to level tile size

--- src/components/cmp_combat.cc
+++ src/components/cmp_combat.cc
@@ -3,6 +3,7 @@
 
 #include <iostream>
 
+#include <level_loader.hh>
 #include <scene.hh>
 
 CmpCombat::CmpCombat(Entity* p) : Component(p), ammo_(5) {}
@@ -17,18 +18,20 @@ void CmpCombat::fire(sf::Vector2f direction)
 
 	std::cout << "FIRE" << std::endl;
 
+	auto size = sf::Vector2f(level::tile_size(), level::tile_size()) / 64.0f;
 
 	auto e = parent_->scene->make_entity();
-	auto s = e->add_component<CmpShape>();
-	auto t = e->add_component<CmpTorpedo>(direction);
-	s->use_shape<sf::CircleShape>(12.f);
-	s->shape().setFillColor(sf::Color::Red);
 	e->move_to(parent_->position());
+
+	auto s = e->add_component<CmpShape>();
+	s->use_shape<sf::CircleShape>(size.x);
+	s->shape().setOrigin(size);
+	s->shape().setFillColor(sf::Color::Red);
+
+	auto t = e->add_component<CmpTorpedo>(direction);
 
 	--ammo_;
 	fire_cooldown_ = 2.0f;
-
-
 }
 
 // checks ammo
--- src/scenes/scene_test.cc
+++ src/scenes/scene_test.cc
@@ -10,21 +10,25 @@ void SceneTest::load()
 {
 	level::load("res/levels/maze.txt");
 
-	sf::Vector2f position(
-		level::tile_position(
-			level::find_tiles(level::Tile::Start).front())
-		+ sf::Vector2f(level::tile_size(), level::tile_size()) / 2.0f);
+	{
+		sf::Vector2f position =
+			level::tile_position(level::find_tiles(
+				level::Tile::Start).front())
+			+ sf::Vector2f(level::tile_size(), level::tile_size())
+				/ 2.0f;
+		sf::Vector2f size = sf::Vector2f(level::tile_size(), level::tile_size()) / 8.0f;
 
-	auto p = make_entity();
-	p->move_to(position);
+		auto p = make_entity();
+		p->move_to(position);
 
-	auto s = p->add_component<CmpShape>();
-	s->use_shape<sf::RectangleShape>(sf::Vector2f(10.0f, 10.0f));
-	s->shape().setOrigin(sf::Vector2f(5.0f, 5.0f));
-	s->shape().setFillColor(sf::Color::Yellow);
+		auto s = p->add_component<CmpShape>();
+		s->use_shape<sf::RectangleShape>(size);
+		s->shape().setOrigin(size / 2.0f);
+		s->shape().setFillColor(sf::Color::Yellow);
 
-	auto m = p->add_component<CmpMovementPlayer>();
-	auto c = p->add_component<CmpCombatPlayer>();
+		p->add_component<CmpMovementPlayer>();
+		p->add_component<CmpCombatPlayer>();
+	}
 
 //	std::this_thread::sleep_for(std::chrono::milliseconds(4444));
 	loaded(true);
